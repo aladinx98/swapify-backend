e73571490253ac37a2b9b7ca9103f86bfe69ef26
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-03-07 18:05:37 +0530

    again update cors

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const UserRoute = require("./src/router/Users");
-const mongoose = require("mongoose");
-
-
 require("./src/db/conn");
 dotenv.config();
 const app = express();
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
-app.use(cors(
-  ({
-    origin: 'https://swapfry01.netlify.app',
-    credentials: true 
-  })));
 app.use("/users", UserRoute);
 app.get("/", async (req, res) => {
   res.send(`server is run ${process.env.EMAIL}`)
-  console.log("MongoDB URL:", process.env.MONGO_URL);
 })
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
