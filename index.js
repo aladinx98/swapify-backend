9d5ed83f99709229142228e81c002e00a9d875db
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-03-07 17:55:47 +0530

    cors file update

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ require("./src/db/conn");
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors(
+  ({
+    origin: 'http://localhost:5173',
+    credentials: true 
+  })));
 app.use("/users", UserRoute);
 app.get("/", async (req, res) => {
   res.send(`server is run ${process.env.EMAIL}`)
