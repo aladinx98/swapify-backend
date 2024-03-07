5070ddde4f8ebf348fde705042abbdd5f82b6a11
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-03-07 18:00:36 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   ({
-    origin: 'http://localhost:5173',
+    origin: 'https://swapfry01.netlify.app',
     credentials: true 
   })));
 app.use("/users", UserRoute);
