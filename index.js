6dac2c12ce157ee93a6df16d467d7de1294cb046
bigban-ui <157366485+bigban-ui@users.noreply.github.com>
2024-03-07 16:53:53 +0530

    console log add

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const UserRoute = require("./src/router/Users");
+const mongoose = require("mongoose");
+
+
 require("./src/db/conn");
 dotenv.config();
 const app = express();
@@ -10,6 +13,7 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use("/users", UserRoute);
 app.get("/", async (req, res) => {
   res.send(`server is run ${process.env.EMAIL}`)
+  console.log("MongoDB URL:", process.env.MONGO_URL);
 })
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
